9ac26bb1c3f01273fc6f98ded1f2d741266cd6f5
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-06-22 14:06:09 +0200

    Get transferlists working again
    Transferlists are now working again. In order to specify a transferlist from the *worker*, you need to call the `transfer()` function and specify what has to be transferred. Note that it would be useful to have a function that extracts what should be transferred from an object automatically!

--- examples/transfer/child.js
+++ examples/transfer/child.js
@@ -1,9 +1,13 @@
 'use strict';
 
-module.exports = function (size, useList, callback) {
-	let arr = new Float64Array(size[0])
-	for (let i = 0; i < arr.length; i++) {
-		arr[i] = Math.random()
-	}
-	callback(null, arr, useList ? [arr.buffer] : null)
-}
+module.exports = function({ data, list }, { transfer }) {
+  let [size] = data;
+  let arr = new Float64Array(size);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Math.random();
+  }
+  if (list) {
+    transfer(arr.buffer);
+  }
+  return arr;
+};
--- examples/transfer/index.js
+++ examples/transfer/index.js
@@ -1,40 +1,27 @@
-'use strict'
+'use strict';
+const workerFarm = require('worker-farm');
+const workers = workerFarm(require.resolve('./child.js'));
+const n = 100;
+const size = 5*1024*1024;
 
-// Beware! Run with node 12.x or higher, or 10.5 with --experimental-worker
-let workerFarm = require('../../')
-  , workers    = workerFarm.threaded(require.resolve('./child'))
-  , ret				 = 0
-  , n 				 = 100
-  , size			 = 5*1024*1024
+(async function() {
 
-;(async function() {
+  console.time('With transferList');
+  for (let i = 0; i < n; i++) {
+    let data = new Uint32Array(size);
+    data[0] = size;
+    await workers({ data, list: true }, [data.buffer]);
+  }
+  console.timeEnd('With transferList');
 
-	console.time('With transferList')
-	await new Promise(function(resolve, reject) {
-		for (let i = 0; i < n; i++) {
-			let data = new Uint32Array([size])
-		  workers(data, true, function(err, result) {
-		  	if (++ret == n)
-		  		resolve()
-		  }, [data.buffer])
-		}
-	})
-	console.timeEnd('With transferList')
+  console.time('Without transferList');
+  for (let i = 0; i < n; i++) {
+    let data = new Uint32Array(size);
+    data[0] = size;
+    await workers({ data, list: false });
+  }
+  console.timeEnd('Without transferList');
 
-	ret = 0
+  workerFarm.end(workers);
 
-	console.time('Without transferList')
-	await new Promise(function(resolve, reject) {
-		for (let i = 0; i < n; i++) {
-			let data = new Uint32Array([size])
-			workers(data, false, function(err, result) {
-				if (++ret == n)
-					resolve()
-			})
-		}
-	})
-	console.timeEnd('Without transferList')
-
-	workerFarm.end(workers)
-
-})().catch(console.error)
+})().catch(console.error);
